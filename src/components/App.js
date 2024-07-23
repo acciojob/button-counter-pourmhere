975f918c5f2aeff4a8105c399450419a98b1f205
Mustafa <pourmhere@gmail.com>
2024-07-24 00:24:07 +0530

    changes done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [cnt, setCnt] = useState(0);
+
+  function cntHandle() {
+    setCnt(cnt + 1);
+    // console.log("Button clicked");
+    // alert("Button clicked");
+    // Add your code here to handle button click event
+    // For example, you can make API calls or update state based on button click event
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <p>Button clicked {cnt}</p>
     </div>
   )
 }
